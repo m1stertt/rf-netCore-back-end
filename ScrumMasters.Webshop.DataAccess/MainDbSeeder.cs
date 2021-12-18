0aa952ac4b0c05c8b0f6b754530c9b43de311545
m1stertt <52206366+m1stertt@users.noreply.github.com>
2021-12-18 02:26:05 +0100

    Ensure created mainDb in production.

--- ScrumMasters.Webshop.DataAccess/MainDbSeeder.cs
+++ ScrumMasters.Webshop.DataAccess/MainDbSeeder.cs
@@ -33,21 +33,21 @@ namespace ScrumMasters.Webshop.DataAccess
                     ProductName = "P3",ProductPrice=350, Categories = new List<CategoryEntity>(), Sizes = new List<SizeEntity>(),
                     Colors = new List<ColorEntity>(), Images = new List<ImageEntity>()
                 };
-
+                
                 CategoryEntity ce1 = new CategoryEntity {Name = "Bukser"};
                 CategoryEntity ce2 = new CategoryEntity {Name = "Sko"};
                 CategoryEntity ce3 = new CategoryEntity {Name = "Kjoler"};
-
+                
                 ColorEntity color1 = new ColorEntity {Title = "Rød",ColorString="#FF0000"};
                 ColorEntity color2 = new ColorEntity {Title = "Blå",ColorString="#0000FF"};
                 ColorEntity color3 = new ColorEntity {Title = "Gul",ColorString="#FFFF00"};
                 ColorEntity color4 = new ColorEntity {Title = "Grøn",ColorString="#00FF00"};
                 ColorEntity color5 = new ColorEntity {Title = "Grå",ColorString="#808080"};
-
+                
                 SizeEntity se1 = new SizeEntity {Title = "30/30"};
                 SizeEntity se2 = new SizeEntity {Title = "25"};
                 SizeEntity se3 = new SizeEntity {Title = "30"};
-
+                
                 ImageEntity ie1 = new ImageEntity
                 {
                     Title = "Some title",
@@ -86,7 +86,7 @@ namespace ScrumMasters.Webshop.DataAccess
                 pe1.Sizes.Add(se1);
                 pe1.Images.Add(ie1);
                 pe1.Images.Add(ie4);
-
+                
                 pe2.Categories.Add(ce2);
                 pe2.Colors.Add(color2);
                 pe2.Colors.Add(color1);
@@ -99,15 +99,15 @@ namespace ScrumMasters.Webshop.DataAccess
                 pe3.Colors.Add(color4);
                 pe3.Sizes.Add(se3);
                 pe3.Images.Add(ie3);
-
-
+                
+                
                 mainContext.Products.AddRange(pe1, pe2, pe3);
                 mainContext.SaveChanges();
         }
 
         public void SeedProduction()
         {
-
+            mainContext.Database.EnsureCreated();
         }
     }
 }
